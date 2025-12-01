128e638f4f8cf9a36d1c49d8158696c2042b7de9
pepperonirollz <cfitzpatrick616@gmail.com>
2025-12-01 10:10:07 -0500

    day one complete

---
+++ aoc-2025/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"github.com/pepperonirollz/aoc-2025/aoc"
+)
+
+func main() {
+	day1Part1Result := aoc.Part1("inputs/day1.txt")
+	day1Part2Result := aoc.Part2("inputs/day1.txt")
+
+	fmt.Printf("Day 1 - Part 1: %d\n", day1Part1Result)
+	fmt.Printf("Day 1 - Part 2: %d\n", day1Part2Result)
+}
